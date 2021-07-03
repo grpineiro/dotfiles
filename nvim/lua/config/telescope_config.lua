90d9427661d7381aa3fe07ddb60032f950bddd9b
Gabriel Piñeiro <grpineiro@gmail.com>
2021-07-03 14:50:03 -0300

    Added telescope configuration file.

---
+++ nvim/lua/config/telescope_config.lua
@@ -0,0 +1,12 @@
+local actions = require('telescope.actions')
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+    file_ignore_patterns = {"node_modules"},
+  }
+}
