2c7c4c908203b2ceb53cb0e9a37121d8eb3bd808
Gabriel Piñeiro <grpineiro@gmail.com>
2021-07-03 14:52:02 -0300

    Added init.lua file, responsable for execute lua config file of the plugins.

---
+++ nvim/lua/config/init.lua
@@ -0,0 +1,7 @@
+require('config/lualine_config')
+require('config/lsp_config')
+require('config/lspsaga_config')
+require('config/lualine_config')
+require('config/telescope_config')
+require('config/tree_sitter_config')
+
