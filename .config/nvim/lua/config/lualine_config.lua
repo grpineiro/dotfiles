69494d81fabc4fb29d1377816b1aaab6ac04965e
Gabriel Piñeiro <grpineiro@gmail.com>
2022-03-23 18:21:19 -0300

    Updated lualine config, added a new style.

--- .config/nvim/lua/config/lualine_config.lua
+++ .config/nvim/lua/config/lualine_config.lua
@@ -1,30 +1,31 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'onedark',
-    section_separators = {'', ''},
-    component_separators = {'', ''},
+    theme = 'nord',
+    section_separators = { left = '', right = ''},
+    component_separators = '|',
     disabled_filetypes = {}
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_a = {{ 'mode', separator = { left = '' }, right_padding = 2 }},
+    lualine_b = { 'filename', { 'branch', icons_enabled = true, icon = "" } },
+    lualine_c = {'fileformat'},
     lualine_x = {
       { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
       'encoding',
-      'filetype'
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      {'location', separator = { right = '' }, left_padding = 2 },
+    }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {'location'}
   },
   tabline = {},
   extensions = {'fugitive'}
