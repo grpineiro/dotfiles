9fa6d3efa296802a07d3d131ac17526d51d7c4cf
Gabriel Piñeiro <grpineiro@gmail.com>
2022-03-28 00:47:11 -0300

    Added nyxt browser config folder.

---
+++ .config/nyxt/auto-config.lisp
@@ -0,0 +1,12 @@
+(define-configuration buffer
+  ((default-modes (append '(nyxt::vi-normal-mode) %slot-default%))))
+(define-configuration buffer
+  ((default-modes (append '(nyxt::emacs-mode) %slot-default%))))
+(define-configuration buffer
+  ((default-modes %slot-default%)))
+(define-configuration buffer
+  ((default-modes (append '(nyxt::vi-normal-mode) %slot-default%))))
+(define-configuration buffer
+  ((default-modes (append '(nyxt::vi-normal-mode) %slot-default%))))
+(define-configuration buffer
+  ((default-modes (append '(nyxt::vi-normal-mode) %slot-default%))))
