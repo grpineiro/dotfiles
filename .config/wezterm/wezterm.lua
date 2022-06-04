c8f1490cc3fdc3c937896a1ffd6689e8aba31776
Gabriel Piñeiro <grpineiro@gmail.com>
2022-06-04 16:15:54 -0300

    Updated wezterm config.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,10 +3,13 @@ local wezterm = require 'wezterm';
 return {
   -- You can specify some parameters to influence the font selection;
   -- for example, this selects a Bold, Italic font variant.
-  font = wezterm.font("JetBrainsMono Nerd Font Mono", {weight="Bold", italic=false}),
+  font = wezterm.font("JetBrains Mono", {weight="Bold", italic=false}),
   color_scheme = "nord",
   hide_tab_bar_if_only_one_tab = true,
-  window_background_opacity = 0.6,
+--  window_background_opacity = 0.6,
+  keys = {
+    { key="n", mods="SHIFT|CTRL", action="ToggleFullScreen" },
+  },
 
   window_padding = {
     left = 2,
