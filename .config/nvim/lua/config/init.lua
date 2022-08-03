4bf2d25a9d2e1188191939ecf252e18eed882314
Gabriel Piñeiro <grpineiro@gmail.com>
2022-08-03 09:45:42 -0300

    Renamed configs.

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,13 +1,53 @@
-require('config/lualine_config')
-require('config/lsp_config')
-require('config/lspsaga_config')
-require('config/lualine_config')
-require('config/telescope_config')
-require('config/tree_sitter_config')
-require('config/compe_config')
+require('config/lualine')
+require('config/lsp')
+require('config/lspsaga')
+require('config/lualine')
+require('config/telescope')
+require('config/treesitter')
+require('config/compe')
 require('config/comment')
 require('config/gitsigns')
+require('config/repl')
 
 -- Init Plugins
 require('nvim-autopairs').setup()
 
+vim.opt.termguicolors = true
+
+-- Attaches to every FileType mode
+require 'colorizer'.setup()
+
+-- Attach to certain Filetypes, add special configuration for `html`
+-- Use `background` for everything else.
+require 'colorizer'.setup {
+  'css';
+  'javascript';
+  html = {
+    mode = 'foreground';
+  }
+}
+
+-- Use the `default_options` as the second parameter, which uses
+-- `foreground` for every mode. This is the inverse of the previous
+-- setup configuration.
+require 'colorizer'.setup({
+  'css';
+  'javascript';
+  html = { mode = 'background' };
+}, { mode = 'foreground' })
+
+-- Use the `default_options` as the second parameter, which uses
+-- `foreground` for every mode. This is the inverse of the previous
+-- setup configuration.
+require 'colorizer'.setup {
+  '*'; -- Highlight all files, but customize some others.
+  css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+  html = { names = false; } -- Disable parsing "names" like Blue or Gray
+}
+
+-- Exclude some filetypes from highlighting by using `!`
+require 'colorizer'.setup {
+  '*'; -- Highlight all files, but customize some others.
+  '!vim'; -- Exclude vim from highlighting.
+  -- Exclusion Only makes sense if '*' is specified!
+}
