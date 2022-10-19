126240c4ad146fd4d37bcf3617e41b9a7f2a7dfd
Gabriel Piñeiro <grpineiro@gmail.com>
2022-10-19 16:17:08 -0300

    Updated wezterm config.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,12 @@
 local wezterm = require 'wezterm';
 
 return {
+  initial_cols=138,
+  initial_rows=42,
   -- You can specify some parameters to influence the font selection;
   -- for example, this selects a Bold, Italic font variant.
-  font = wezterm.font("JetBrains Mono", {weight="Bold", italic=false}),
+  font = wezterm.font("JetBrains Mono", {weight="Regular", italic=false}),
+  font_size=13.0,
   color_scheme = "nord",
   hide_tab_bar_if_only_one_tab = true,
 --  window_background_opacity = 0.6,
@@ -16,5 +19,12 @@ return {
     right = 2,
     top = 0,
     bottom = 0,
-  }
+  },
+
+  enable_scroll_bar = true,
+
+  default_cursor_style = "BlinkingBlock",
+  cursor_blink_rate = 800,
+  anti_alias_custom_block_glyphs = false,
+  enable_kitty_graphics=true
 }
