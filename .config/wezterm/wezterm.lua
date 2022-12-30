db8dba4975e09a6deaba7d053ac15635d957e3b3
Gabriel Piñeiro <grpineiro@gmail.com>
2022-12-30 18:50:19 -0300

    Updated wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require 'wezterm';
 
 return {
-  initial_cols=138,
+  initial_cols=142,
   initial_rows=42,
   -- You can specify some parameters to influence the font selection;
   -- for example, this selects a Bold, Italic font variant.
   font = wezterm.font("JetBrains Mono", {weight="Regular", italic=false}),
   font_size=13.0,
-  color_scheme = "nord",
+  color_scheme = "carbonfox",
   hide_tab_bar_if_only_one_tab = true,
 --  window_background_opacity = 0.6,
   keys = {
