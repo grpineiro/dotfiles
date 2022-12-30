3c972dfefa800df06dca7f81caa466e871d23329
Gabriel Piñeiro <grpineiro@gmail.com>
2022-12-30 18:45:59 -0300

    Updated lualine.

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -4,7 +4,6 @@ if not status then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'nord',
     section_separators = { left = '', right = ''},
     component_separators = '|',
     disabled_filetypes = {},
