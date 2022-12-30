a499f94162f457588032d5ebfad82ba2cbbb047a
Gabriel Piñeiro <grpineiro@gmail.com>
2022-12-30 18:41:53 -0300

    "Rename to theme.lua"

--- .config/nvim/lua/core/background.lua
+++
@@ -1,9 +0,0 @@
-local status, nord = pcall(require, "nord")
-if not status then return end
-
-vim.g.nord_italic = false
-vim.g.nord_disable_background = false
-vim.g.nord_borders = true
-vim.g.nord_contrast = true
-
-nord.set()
