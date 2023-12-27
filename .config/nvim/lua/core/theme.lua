55248c21bc8fac05e6d236bb03b2f1b9b50eb369
Gabriel Piñeiro <grpineiro@gmail.com>
2023-12-27 19:45:32 -0300

    Added Github Dark theme.

--- .config/nvim/lua/core/theme.lua
+++ .config/nvim/lua/core/theme.lua
@@ -1,11 +1,14 @@
-local status, theme = pcall(require, 'catppuccin')
+-- local status, theme = pcall(require, 'catppuccin')
+-- if not status then return end
+
+local status, theme = pcall(require, 'github-theme')
 if not status then return end
 
-theme.setup {
-  flavour = "mocha",
-  background = {
-    dark = "mocha",
-  },
-}
+-- theme.setup {
+--   flavour = "mocha",
+--   background = {
+--     dark = "mocha",
+--   },
+-- }
 
-vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme github_dark_default")
